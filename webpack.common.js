98f586267ca4d925d81a28a7b4814169972c5a2a
bryan0147 <bryan0147@gmail.com>
2019-08-12 19:00:24 -0400

    Initial file structure setup

---
+++ webpack.common.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const outputDir = "./dist";
+
+module.exports = {
+  entry: path.resolve(__dirname, "src", "index.js"),
+  output: {
+    path: path.join(__dirname, outputDir),
+    filename: "bundle.js",
+    publicPath: "./dist/"
+  },
+  resolve: {
+    extensions: [".js"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: { presets: ["env"] }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          use: ["css-loader", "postcss-loader"],
+          fallback: "style-loader"
+        })
+      },
+      {
+        test: /\.scss/,
+        use: ExtractTextPlugin.extract({
+          use: ["css-loader", "sass-loader", "postcss-loader"],
+          fallback: "style-loader"
+        })
+      }
+    ]
+  },
+  plugins: [new ExtractTextPlugin("bunde_style.css"), require("autoprefixer")],
+  // devtool: 'eval-source-map'
+};
