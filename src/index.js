f55234f7a80b4bdaef9f3b29c2c5cd525837cea5
bryan0147 <bryan0147@gmail.com>
2019-08-13 17:42:30 -0400

    Finish background. Finish styling title

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-console.log("this est")
+// fetch('https://api.le-systeme-solaire.net/rest/bodies/terre')
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     const name = data.englishName
+//     document.getElementById("name").innerHTML = name
+//   });
 
-fetch('https://api.le-systeme-solaire.net/rest/bodies/terre')
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    const name = data.englishName
-
-    
-    document.getElementById("name").innerHTML = name
-  });
+// < !-- < h1 id = "name" ></h1 > -->
--- src/javascripts/solarsystem.js
+++ src/javascripts/solarsystem.js
@@ -1,6 +1,19 @@
 const svgContainer = d3.select("body").append("svg")
+  .attr('class', 'svgContainer')
  
+const starsContainer = svgContainer.append("g")
+  .attr('class', 'stars')
 
-const backgroundRect = svgContainer.append("rect")
+function generateStars(number) {
+  const stars = starsContainer.selectAll("circle")
+    .data(d3.range(number).map(d =>
+      i = { x: Math.random() * (2100), y: Math.random() * (2100), r: Math.random() * 1 }
+    ))
+    .enter().append("circle")
+    .attr("class", "star")
+    .attr("cx", d => d.x)
+    .attr("cy", d => d.y)
+    .attr("r", d => d.r);
+}
 
-const test = svgContainer.append("rect")
+generateStars(1000);
